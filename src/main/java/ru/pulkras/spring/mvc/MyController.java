67bb1c7b0da0b242bdb98202cbd6e4d8035214fc
pulkras <maligin128@gmail.com>
2023-02-23 23:56:30 +0300

    first spring mvc project

---
+++ src/main/java/ru/pulkras/spring/mvc/MyController.java
@@ -0,0 +1,14 @@
+package ru.pulkras.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "firstView";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/firstView.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h1>this is my page</h1>
+
+</body>
+
+</html>
